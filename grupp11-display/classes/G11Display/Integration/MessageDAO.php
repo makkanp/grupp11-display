c4b44cc2d63a6e680d7e0a72577a5a0a21d7c5d4
Markus <markus.palmqvist@hotmail.com>
2017-04-19 13:26:53 +0200

    Uppdatering av MessageDAO

--- grupp11-display/classes/G11Display/Integration/MessageDAO.php
+++ grupp11-display/classes/G11Display/Integration/MessageDAO.php
@@ -6,15 +6,17 @@
  * Date: 2017-04-18
  * Time: 14:35
  */
-class MessageDAO {
-    private $dbobj;
-    private $db;
-    private $display;
+use G11Display\Integration\DBinfo as DBI;
+class MessageDAO  {
 
-    public function __construct($display)    {
-        $this->dbobj = new DBinfo();
-        $this->db = $this->dbobj->getDB();
-        $this->recipe = $display;
+    private $mysqli = null;
+
+    public function __construct(){
+        if($this->mysqli == null)
+            $this->mysqli = new mysqli(DBI::HOST, DBI::USERNAME, DBI::PASSWORD, DBI::DB);
+
+        if ($this->mysqli->error)
+            throw new Exception($this->mysqli->error);
     }
 
     public function createNewMessage($message) {
@@ -22,46 +24,38 @@ class MessageDAO {
        // if(!(ctype_print($message))) //bör kanske ligga i view eller controller???
         //    return ['error' => 'Invalid message'];
 
-        $stmt = $this->db->prepare("INSERT INTO messages (message) VALUES (?)");
+        $stmt = $this->mysqli->prepare("INSERT INTO messages ($message) VALUES (?)");
         $stmt->bind_param("s", $message);
         $stmt->execute();
-    }
+
+
+            if ($this->mysqli->affected_rows != 1){
+                die("Misslyckades med insättning av värde");
+            }
+
+
+        }
+
 
     public function getMessage()    {
         $sql = "SELECT * 
                 FROM display 
                 ORDER BY cid DESC";
-        $result = $this->db->query($sql);
+        $result = $this->mysqli->query($sql);
 
-        return $result;
-        // while
+        if($result == false) {
+            die("Result är tomt");
+        }
+        //while (($row = $result->fetch_row()) != NULL)
+        $row = $result->fetch_row();
+
+        return $row[1];
     }
 
     public function __destruct()
     {
-        close();
+        $this->mysqli->close();
     }
 }
 
 
-class DBinfo
-{
-    private static $host = '127.0.0.1';
-    private static $username = 'root';
-    private static $password = '';
-    private static $db = 'II1302';
-
-    private $mysqli = null;
-
-    public function __construct(){
-        if($this->mysqli == null)
-            $this->mysqli = new mysqli(self::$host, self::$username, self::$password, self::$db);
-
-        if ($this->mysqli->error)
-            throw new Exception($this->mysqli->error);
-    }
-
-    public function getConnection(){
-        return $this->mysqli;
-    }
-}
