15b8778f970b682593ee3e1c1d7078ab930db211
Markus <markus.palmqvist@hotmail.com>
2017-04-19 13:35:52 +0200

    Uppdatering av Controller

--- grupp11-display/classes/G11Display/Controller/Controller.php
+++ grupp11-display/classes/G11Display/Controller/Controller.php
@@ -6,17 +6,25 @@
  * Date: 2017-04-18
  * Time: 16:24
  */
+
+
 class Controller
 {
+    private $messageDAO;
+
+    public function __construct() {
+        $this->messageDAO = new MessageDAO();
+    }
+
     public function createNewMessage($message){
 
 
-        return $this->MessageDAO->createNewMessage($message);
+        return $this->messageDAO->createNewMessage($message);
     }
 
 
     public function getMessage($message){
 
-        return $this->NewMessage->getMessage($message);
+        return $this->messageDAO->getMessage($message);
     }
 }
